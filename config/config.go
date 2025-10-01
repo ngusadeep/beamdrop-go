1c64f3fcbf36d396b5f1356660d2cb93eacde966
Tachera Sasi <tacherasasi@gmail.com>
2025-10-02 00:18:56 +0300

    feat: implement configuration directory creation and default config setup

--- config/config.go
+++ config/config.go
@@ -1,7 +1,21 @@
 package config
 
-const PORT = 7777
-const VERSION = "0.0.1"
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
+
+const (
+	PORT          = 7777
+	VERSION       = "0.0.1"
+	ConfigDirName = ".beamdrop"
+)
+
+var (
+	ConfigDir  string
+	ConfigPath string
+)
 
 type Config struct {
 	PORT int
@@ -19,6 +33,50 @@ func GetConfig() Config {
 	}
 }
 
-func createConfigDir(){
-	
+func init() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("failed to get home directory: %v", err)
+	}
+	ConfigDir = filepath.Join(homeDir, ConfigDirName)
+	ConfigPath = filepath.Join(ConfigDir, "config.json")
+
+	createConfigDir()
+
+	if _, err := os.Stat(ConfigPath); os.IsNotExist(err) {
+		createDefaultConfig()
+	} else {
+		loadExistingConfig()
+	}
 }
+
+func createConfigDir() {
+	if err := os.MkdirAll(ConfigDir, 0755); err != nil {
+		log.Fatalf("failed to create config directory: %v", err)
+	}
+}
+
+func createDefaultConfig() {
+	// For now, we'll create an empty config file
+	// In the future, you can serialize the config struct to JSON/YAML
+	file, err := os.Create(ConfigPath)
+	if err != nil {
+		log.Fatalf("failed to create config file: %v", err)
+	}
+	defer file.Close()
+	
+	// Write a simple comment for now
+	_, err = file.WriteString("# BeamDrop Configuration\n")
+	if err != nil {
+		log.Fatalf("failed to write to config file: %v", err)
+	}
+	
+	log.Printf("Created default config file at: %s", ConfigPath)
+}
+
+func loadExistingConfig() {
+	// For now, just log that we're loading the existing config
+	// In the future, you can parse the config file and load settings
+	log.Printf("Loading existing config from: %s", ConfigPath)
+}
+
